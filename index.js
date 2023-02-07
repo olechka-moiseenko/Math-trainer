d73c1f17fba5af15daeeed8e67d1f3feb63b5c3a
Olechka Moiseenko <olechka.moiseenko@gmail.com>
2023-02-08 00:04:10 +0300

    Add styles for mobile version

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const math_op = document.getElementById("math_operation");
 const result = document.getElementById("example_result");
 const corAnsw = document.getElementById("correct_answer");
 const attempts = document.getElementById("attempts_counter");
+const modalWindow = document.getElementById("modal_window");
+const closeButton = document.querySelector(".modal_btn");
+const modalWindOverlay = document.querySelector("modal_overlay");
+const titleModal = document.getElementById("modal_title");
+const imgModal = document.getElementById("modal_image");
+
+closeButton.addEventListener("click", () => {
+  modalWindow.classList.remove("is-open");
+});
 
 function calculate(operator, a, b) {
   if (operator == "+") {
@@ -25,18 +34,16 @@ let correctAnswers = 0;
 
 function checkExample() {
   attemptsCounter++;
-  if (check()) {
+  const res = check();
+  showResultAnswer(res);
+  if (res) {
     correctAnswers++;
-    alert("Верно");
     result.value = null;
     generateExample();
-  } else {
-    alert("Неверно");
   }
   corAnsw.textContent = correctAnswers;
   attempts.textContent = attemptsCounter;
 }
-
 
 function check() {
   const a = parseInt(arg_a.textContent);
@@ -66,7 +73,7 @@ function generateExample() {
     arg_b.textContent = b;
   } else if (math_op.textContent == "-") {
     let b = getRand(100, 1000);
-    if(b > a) {
+    if (b > a) {
       [a, b] = [b, a];
     }
     arg_b.textContent = b;
@@ -76,6 +83,20 @@ function generateExample() {
     arg_b.textContent = getRand(0, 10);
   }
   arg_a.textContent = a;
+}
+
+function showResultAnswer(result) {
+  console.log(`img.src: ${imgModal.src}`);
+  if (result) {
+    titleModal.textContent = "Молодец! Правильный ответ!";
+    imgModal.src = "./images/super.gif";
+    closeButton.textContent = "Следуйщий пример";
+  } else {
+    titleModal.textContent = "Неправильный ответ. Подумай ещё!";
+    imgModal.src = "./images/sad.gif";
+    closeButton.textContent = "Вернуться к примеру";
+  }
+  modalWindow.classList.add("is-open");
 }
 
 generateExample();
